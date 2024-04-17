e218fbbd3dfa199d682176f084a4efeb848f2d50
Diego <diego.almeida.0779@gmail.com>
2024-04-17 16:29:11 -0300

    finsh

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,83 @@
+﻿/*using System;
+
+namespace PrimeiroProjeto
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int numero1 = 300;
+            int numero2 = 20;
+
+
+            int divisao = numero1 / numero2;
+
+
+            Console.WriteLine("essa é a quantidade de caixas utilizadas: " + divisao);*/
+
+/*using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero01 = 12;
+            int numero02 = 10;
+
+            Console.WriteLine("Seus Numeros são: " + numero01 + " e " + numero02);
+
+            int soma = numero01 + numero02;
+
+            Console.WriteLine("Seu total é: " + soma);
+        }
+    }
+}*/
+
+/*using System;
+
+int numero1 = 27;
+int numero2 = 16;
+
+int subtracao = numero1 - numero2;
+Console.WriteLine("Ela tem: " + subtracao + " Bombons");*/
+
+/*using System;
+
+int dinheiro = 35;
+int horas = 8;
+
+int salario = dinheiro * horas;
+
+Console.WriteLine("Seu salário é :" + salario);*/
+
+/*using System;
+
+namespace CalculadoraTermal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double precoInstalacao = 300.00;
+            double precoLimpeza = 150.00;
+            double precoManutencao = 499.99;
+
+
+            double descontoNovoCliente = 50.00;
+
+
+            double totalServicos = precoInstalacao + precoLimpeza + precoManutencao;
+
+
+            double totalAPagar = totalServicos - descontoNovoCliente;
+
+
+            Console.WriteLine("O total a ser pago pela empresa TERMAL é: R$" + totalAPagar);
+        }
+    }
+}*/
